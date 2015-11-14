606cad18354a747b3be3be08c98162dc949065e3
pisipisi <admin@pipi>
2015-11-13 20:55:53 -0600

    Initial Commit

---
+++ Take Them DownTests/Take_Them_DownTests.swift
@@ -0,0 +1,36 @@
+//
+//  Take_Them_DownTests.swift
+//  Take Them DownTests
+//
+//  Created by Pisi on 11/13/15.
+//  Copyright © 2015 AznSoft. All rights reserved.
+//
+
+import XCTest
+@testable import Take_Them_Down
+
+class Take_Them_DownTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
